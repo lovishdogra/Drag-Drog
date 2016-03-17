984bc63dadf65fcb9db572c87d27585f1e058d39
Lovish Dogra <lovishdogra7@gmail.com>
2016-03-17 15:30:22 +0530

    changes for drag&drop
    Code written in view controller and myView.swift file

--- Drag & Drog/ViewController.swift
+++ Drag & Drog/ViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MyView: myView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let halfSizeOfView = 25.0
+        let maxViews = 25
+        let insetSize = CGRectInset(self.view.bounds, CGFloat(Int(2 * halfSizeOfView)), CGFloat(Int(2 * halfSizeOfView))).size
+        
+        // Add the Views
+        for i in 0..<maxViews {
+            let pointX = CGFloat(UInt(arc4random() %
+                UInt32(UInt(insetSize.width))))
+            let pointY = CGFloat(UInt(arc4random() %
+                UInt32(UInt(insetSize.height))))
+            
+            let newView = myView(frame: CGRectMake(pointX, pointY, 50, 50))
+            self.view.addSubview(newView)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
---
+++ Drag & Drog/myView.swift
@@ -0,0 +1,46 @@
+//
+//  myView.swift
+//  Drag & Drog
+//
+//  Created by Lovish Dogra on 17/03/16.
+//  Copyright © 2016 Lovish Dogra. All rights reserved.
+//
+
+import UIKit
+
+class myView: UIView {
+    
+    var lastLocation:CGPoint = CGPointMake(0, 0)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // Initialization code
+        let panRecognizer = UIPanGestureRecognizer(target:self, action:"detectPan:")
+        self.gestureRecognizers = [panRecognizer]
+        
+        //randomize view color
+        let blueValue = CGFloat(Int(arc4random() % 255)) / 255.0
+        let greenValue = CGFloat(Int(arc4random() % 255)) / 255.0
+        let redValue = CGFloat(Int(arc4random() % 255)) / 255.0
+        
+        self.backgroundColor = UIColor(red:redValue, green: greenValue, blue: blueValue, alpha: 1.0)
+    }
+    
+    func detectPan(recognizer:UIPanGestureRecognizer) {
+        let translation  = recognizer.translationInView(self.superview!)
+        self.center = CGPointMake(lastLocation.x + translation.x, lastLocation.y + translation.y)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        // Promote the touched view
+        self.superview?.bringSubviewToFront(self)
+        
+        // Remember original location
+        lastLocation = self.center
+    }
+}
+
